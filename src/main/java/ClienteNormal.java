8186f3079686f13af514ca9c3f9ad5a50d96be3e
Juarez Campos <42527050+JuarezCampos93@users.noreply.github.com>
2023-12-05 08:34:53 -0300

    Update ClienteNormal.java

--- src/main/java/ClienteNormal.java
+++ src/main/java/ClienteNormal.java
@@ -1,4 +1,4 @@
-public class ConsumidorPremioIsento extends Consumidor {
+public class ClienteNormal extends Cliente{
     public String verificarAvaliacao() {
         if(this.getAvaliacao() >= 7){
             return "Prêmio";
